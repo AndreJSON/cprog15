3ca606b07e0593aa982eaece3dac0c9a2e662deb
Andre Josefsson <andre.v.json@gmail.com>
2015-09-29 16:44:52 +0200

    Adds more constructors and test code for those.

--- lab-1/assignment-7/cxxtest_template_vector.h
+++ lab-1/assignment-7/cxxtest_template_vector.h
@@ -3,8 +3,45 @@
 
 class MyTestSuite : public CxxTest::TestSuite {
 	public:
-		void test1 () { //Testing the regular constructor.
-			UIntVector<char> a((size_t)17);
+		void test1 () {
+			Vector<std::string> a;
+			TS_ASSERT_EQUALS(a.size(), 0);
+		}
+
+		void test2 () { //Testing the size_t constructor.
+			Vector<char> a((size_t)17);
+			TS_ASSERT_EQUALS(a.size(), 17);
+		}
+
+		void test3 () { //Testing the (size_t, value) constructor.
+			Vector<int> a((size_t)17, 5);
+			TS_ASSERT_EQUALS(a.size(), 17);
+			TS_ASSERT_EQUALS(a[0], 5);
+			TS_ASSERT_EQUALS(a[3], 5);
+			TS_ASSERT_EQUALS(a[16], 5);
+		}
+
+		void test4 () { //Testing the initializer_list constructor.
+			Vector<int> a = {1,3,-1,0};
+			TS_ASSERT_EQUALS(a.size(), 4);
+			TS_ASSERT_EQUALS(a[0], 1);
+			TS_ASSERT_EQUALS(a[2], -1);
+			TS_ASSERT_EQUALS(a[3], 0);
+			TS_ASSERT_THROWS(a[4], std::out_of_range);
+		}
+
+		void testX () { //Testing the operator[] overload
+			Vector<unsigned int> a((size_t)10);
+			a[0] = (unsigned int)82;
+			a[5] = (unsigned int)99;
+			a[9] = (unsigned int)10005;
+			TS_ASSERT_EQUALS(a[0], 82);
+			TS_ASSERT_EQUALS(a[5], 99);
+			TS_ASSERT_EQUALS(a[9], 10005);
+			a.reset();
+			TS_ASSERT_EQUALS(a[0], 0);
+			TS_ASSERT_EQUALS(a[0], 0);
+			TS_ASSERT_EQUALS(a[0], 0);
 		}
 };
 
--- lab-1/assignment-7/kth_cprog_template_container.hpp
+++ lab-1/assignment-7/kth_cprog_template_container.hpp
@@ -2,46 +2,113 @@
 #include <iostream>
 
 template <typename T>
-class UIntVector {
+class Vector {
 private:
-	//void resize (size_t const&);
+	void resize (size_t const&);
 	size_t backSize, frontSize;
 	T* array;
 
 public:
-	explicit UIntVector (size_t);
-	UIntVector (std::initializer_list<unsigned int>);
-	UIntVector (UIntVector const&);
-	UIntVector (UIntVector&&) noexcept;
-	~UIntVector ();
-	UIntVector& operator= (UIntVector const&);
-	UIntVector& operator= (UIntVector&&);
-	unsigned int operator[] (size_t const&) const;
-	unsigned int& operator[] (size_t const&);
-	void reset();
-	size_t size() const;
+	explicit Vector ();
+	explicit Vector (size_t);
+	explicit Vector (size_t, T const&);
+	Vector (std::initializer_list<T>);
+	Vector (Vector const&);
+	Vector (Vector&&) noexcept;
+	~Vector ();
+	Vector& operator= (Vector const&);
+	Vector& operator= (Vector&&);
+	T operator[] (size_t const&) const;
+	T& operator[] (size_t const&);
+	void reset ();
+	size_t size () const;
 };
 
+//Default constructor. Makes room for 10 elements.
+template <typename T>
+Vector<T>::Vector () {
+	frontSize = 0;
+	backSize = 10;
+	array = (T*)malloc(sizeof(T) * backSize);
+}
 
 //Constructor taking a size parameter.
 template <typename T>
-UIntVector<T>::UIntVector (size_t size) {
+Vector<T>::Vector (size_t size) {
 	frontSize = size;
 	backSize = size;
-	array = (T*)malloc(/*sizeof(T) **/ size);
-	//reset();
+	array = (T*)malloc(sizeof(T) * backSize);
+}
+
+//Constructor taking a size parameter and a value.
+template <typename T>
+Vector<T>::Vector (size_t size, T const& value) {
+	frontSize = size;
+	backSize = size;
+	array = (T*)malloc(sizeof(T) * backSize);
+	for (auto i = 0; i < frontSize; i++) {
+		array[i] = value;
+	}
+}
+
+//Constructor taking an initializer_list.
+template <typename T>
+Vector<T>::Vector (std::initializer_list<T> il) {
+	frontSize = il.size();
+	backSize = frontSize;
+	array = (T*)malloc(sizeof(T) * backSize);
+	auto i = 0;
+	for (auto n = il.begin(); n != il.end(); n++) {
+		array[i++] = *n;
+	}
 }
 
 //Destructor.
 template <typename T>
-UIntVector<T>::~UIntVector () {
+Vector<T>::~Vector () {
 	free(array);
 }
 
+//Operator [] overload (get value).
+template <typename T>
+T Vector<T>::operator[] (size_t const& index) const {
+	if (index < frontSize) {
+		return array[index];
+	} else {
+		throw std::out_of_range("index out of bounds error.");
+	}
+}
+
+//Operator [] overload (set value).
+template <typename T>
+T& Vector<T>::operator[] (size_t const& index) {
+	if (index < frontSize) {
+		return array[index];
+	} else {
+		throw std::out_of_range("index out of bounds error.");
+	} 
+}
+
+//Resets all elements in the publicly seen part of the vector to be zero initialized.
+template <typename T>
+void Vector<T>::reset () {
+	for (auto i = 0; i < frontSize; i++) {
+		array[i] = (T)0;
+	}
+}
+
+//Gives the size of the publicly seen vector.
+template <typename T>
+size_t Vector<T>::size () const {
+	return frontSize;
+}
+
+
 //Called internally to increase the size of the vector enough to fit the index into it.
-/*void UIntVector::resize(size_t const& index) {
+/*template <typename T>
+void Vector<T>::resize(size_t const& index) {
 	if (index >= backSize) { //allocate double the space of index.
-		unsigned int * tmp = (unsigned int*)malloc(sizeof(unsigned int) * (index<<1)); //Might break if you try elements to a huge index (bigger the size of size_t).
+		T* tmp = (T*)malloc(sizeof(T) * (index<<1)); //Might break if you try elements to a huge index (bigger the size of size_t).
 		
 	} else { //index must have been smaller than backSize but greater than frontSize.
 		frontSize = index;
