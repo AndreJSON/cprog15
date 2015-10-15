79ac5bfd58ff05a80fc18541fe583f756abd4dda
Andre Josefsson <andre.v.json@gmail.com>
2015-10-16 00:14:59 +0200

    Adds kattistime, a program that was handed out to the course student. It can be used to simulate different dates.

--- lab-2/date.hpp
+++ lab-2/date.hpp
@@ -2,7 +2,13 @@
 
 namespace lab2 {
 	class Date {
+	protected:
+		int ejd; //Early Julian Day.
+		std::string day_names[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+		std::string month_names[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		//virtual int is_leap_year(some type) = 0; not fully sure what should be passed here. I will settle that later.
 	public:
+		virtual Date () = 0;
 		virtual int year() = 0;
 		virtual unsigned int month() = 0;
 		virtual unsigned int day() = 0;
